e07a25f47c64e9c25d626a5874d35f83f4555d80
DaveHP-Hub <66231069+DaveHP-Hub@users.noreply.github.com>
2020-12-07 16:52:57 -0600

    Update Service

--- src/app/services/albums.service.ts
+++ src/app/services/albums.service.ts
@@ -21,7 +21,7 @@ querySpotify(query: string)
 {
   const URL = `https://api.spotify.com/v1/playlists/${query}?limit=8`;
   const headers  = new HttpHeaders({
-    'Authorization': 'Bearer BQBSQ1VgIlUJDJmd8xKgtYOV4PQVS9lWjRIB8WD8RlvjXfm32RixW-69ccOt87lY1-G_tSWeZEH0vpXAVgU'
+    'Authorization': 'Bearer myToken'
   });
   return this.http.get(URL, {headers});
 }
